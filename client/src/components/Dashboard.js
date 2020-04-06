e260e9f7201048c71e3f88f49ae5cc7598d86374
Ivan5 <converse_ivan@hotmail.com>
2020-04-06 15:33:23 -0500

    Build Dashboard Component

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Dashboard({ setAuth }) {
+  const [name, setName] = useState("");
+
+  async function getName() {
+    try {
+      const response = await fetch("http://localhost:5000/dashboard/", {
+        method: "GET",
+        headers: { token: localStorage.getItem("token") },
+      });
+
+      const parseRes = await response.json();
+      setName(parseRes.user_name);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  const logout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    setAuth(false);
+  };
+
+  useEffect(() => {
+    getName();
+  });
+
   return (
     <>
-      <h1>Dashboard</h1>
-      <button onClick={() => setAuth(false)}>LogOut</button>
+      <h1>Dashboard {name}</h1>
+      <button className="btn btn-info" onClick={(e) => logout(e)}>
+        Logout
+      </button>
     </>
   );
 }
--- server/middleware/authorization.js
+++ server/middleware/authorization.js
@@ -1,16 +1,16 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-module.exports = async (req, res, next) => {
+module.exports = (req, res, next) => {
+  const jwtToken = req.header("token");
+  if (!jwtToken) {
+    return res.status(403).json("Not Authorize");
+  }
   try {
-    const jwtToken = req.header("token");
-    if (!jwtToken) {
-      return res.status(403).json("Not Authorize");
-    }
-
     const payload = jwt.verify(jwtToken, process.env.jwtSecret);
 
     req.user = payload.user;
+    next();
   } catch (error) {
     console.error(error.message);
     return res.status(403).json("Not Authorize");
