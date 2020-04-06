add59631d23dab5f39fc391e73f963a6c08dcade
Ivan5 <converse_ivan@hotmail.com>
2020-04-06 15:01:39 -0500

    Build Login Component

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Register({ setAuth }) {
   const [inputs, setInputs] = useState({
@@ -64,6 +65,7 @@ export default function Register({ setAuth }) {
         />
         <button className="btn btn-success btn-block">Submit</button>
       </form>
+      <Link to="/login">Login</Link>
     </>
   );
 }
